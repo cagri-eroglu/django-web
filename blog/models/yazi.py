c654d1dfc8e90035220f72bc5cb3e3cf77df3355
Cagri Eroglu <cagrieroglu9@gmail.com>
2022-11-03 17:59:19 +0300

    yazi admin paneli duzenlendi

--- blog/models/yazi.py
+++ blog/models/yazi.py
@@ -2,14 +2,16 @@ from django.db import models
 from autoslug import AutoSlugField
 from blog.models import KategoriModel
 from django.contrib.auth.models import User
+from ckeditor.fields import RichTextField
 
 
 
 class YazilarModel(models.Model):
+    resim=models.ImageField(upload_to='yazi_resimleri',null=True)
     baslik=models.CharField(max_length=50)
-    icerik=models.TextField()
+    icerik=RichTextField()
     olusturulma_tarihi = models.DateTimeField(auto_now_add=True)
-    duzenlenme_tarihi=models.DataTimeField(auto_now=True)
+    duzenlenme_tarihi=models.DateTimeField(auto_now=True)
     slug=AutoSlugField(populate_from='baslik',unique=True)
     kategoriler=models.ManyToManyField(KategoriModel,related_name='yazilar')
     yazar = models.ForeignKey(User, on_delete=models.CASCADE,related_name='yazilar')
@@ -17,4 +19,7 @@ class YazilarModel(models.Model):
     class Meta:
         verbose_name='Yazi'
         verbose_name_plural='Yazilar'
-        db_name='Yazi'
+        db_table='Yazi'
+    def __str__(self):
+        return self.baslik
+
