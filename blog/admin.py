a85ff2b8e824029e0e82cf43e2c0463c873b26f2
Cagri Eroglu <cagrieroglu9@gmail.com>
2022-11-03 18:58:20 +0300

    iletişim paneli ve admin paneli ayarlari yapildi

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from blog.models import KategoriModel,YazilarModel,YorumModel
+from blog.models import KategoriModel,YazilarModel,YorumModel,IletisimModel
 
 admin.site.register(KategoriModel)
 
@@ -16,3 +16,9 @@ class YorumAdmin(admin.ModelAdmin):
     search_fields=('yazan__username',)
 
 admin.site.register(YorumModel,YorumAdmin)
+
+class IletisimAdmin(admin.ModelAdmin):
+    list_display=('email','olusturulma_tarihi')
+    search_fields=('email',)
+
+admin.site.register(IletisimModel,IletisimAdmin)
