8ebad91c5a212a1ffc64df2823c13828b77d71c5
Cagri Eroglu <cagrieroglu9@gmail.com>
2022-11-03 17:21:06 +0300

    yazilar modeli olusturuldu

---
+++ blog/models/yazi.py
@@ -0,0 +1,20 @@
+from django.db import models
+from autoslug import AutoSlugField
+from blog.models import KategoriModel
+from django.contrib.auth.models import User
+
+
+
+class YazilarModel(models.Model):
+    baslik=models.CharField(max_length=50)
+    icerik=models.TextField()
+    olusturulma_tarihi = models.DateTimeField(auto_now_add=True)
+    duzenlenme_tarihi=models.DataTimeField(auto_now=True)
+    slug=AutoSlugField(populate_from='baslik',unique=True)
+    kategoriler=models.ManyToManyField(KategoriModel,related_name='yazilar')
+    yazar = models.ForeignKey(User, on_delete=models.CASCADE,related_name='yazilar')
+
+    class Meta:
+        verbose_name='Yazi'
+        verbose_name_plural='Yazilar'
+        db_name='Yazi'
